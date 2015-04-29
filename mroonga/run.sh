cf390946be400eba44be37251279383076fdbaa1
Hideharu Ohkuma <ohkuma@33i.co.jp>
2015-04-29 17:38:05 +0900

    mroonga image

--- mroonga/run.sh
+++ mroonga/run.sh
@@ -1,41 +1,31 @@
 #!/bin/bash
 
-MYSQL_EXEC_USER=${MYSQL_EXEC_USER:-mysql}
+MYSQL_ROOT_PASSWORD=${MYSQL_ROOT_PASSWORD:-rootpassword}
 MYSQL_USER=${MYSQL_USER:-user}
-MYSQL_USER_PASSWORD=${MYSQL_USER_PASSWORD:-asdfg}
-MYSQL_ROOT_PASSWORD=${MYSQL_ROOT_PASSWORD:-qwerty}
+MYSQL_USER_PASSWORD=${MYSQL_USER_PASSWORD:-password}
 
-if [ "$1" = 'mysqld' ]; then
-  if [ ! -d "/var/lib/mysql/mysql" ]; then
-    echo "Running mysql_install_db ..."
-    mysql_install_db --datadir="/var/lib/mysql"
-    echo "Finished mysql_install_db"
-    
-    tempSqlFile='/tmp/mysql-init.sql'
-    cat > "$tempSqlFile" <<EOSQL
-      DELETE FROM mysql.user;
-      CREATE USER 'root'@'%' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}' ;
-      GRANT ALL ON *.* TO 'root'@'%' WITH GRANT OPTION ;
-      DROP DATABASE IF EXISTS test ;
-EOSQL
+if [ "$1" = "/usr/sbin/mysqld" ]; then
+  if [ ! -e "/var/lib/mysql/data" ]; then
+    echo "mysql_install_db..."
+    mysql_install_db
+    chown -R mysql:mysql /var/lib/mysql
+    echo "done"
 
-    if [ "$MYSQL_USER" -a "$MYSQL_USER_PASSWORD" ]; then
-      echo "CREATE USER '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_USER_PASSWORD' ;" >> "$tempSqlFile"
-      echo "GRANT ALL ON *.* TO '$MYSQL_USER'@'%';" >> "$tempSqlFile"
-    fi
+    echo "Set up mysqld..."
+    /usr/sbin/mysqld --user=mysql &
+    mysql_pid=$!
+    sleep 5
+    mysql -u root < /usr/share/mroonga/install.sql
+    /usr/bin/mysqladmin password $MYSQL_ROOT_PASSWORD -u root
+    kill $mysql_pid
+    sleep 3
+    echo "done"
 
-    echo 'FLUSH PRIVILEGES;' >> "$tempSqlFile"
-    set -- "$@" --init-file="$tempSqlFile"
+    echo "CREATE USER '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_USER_PASSWORD' ;" >> /tmp/mysql-init.sql
+    echo "GRANT ALL ON *.* TO '$MYSQL_USER'@'%';" >> /tmp/mysql-init.sql
+
+    set "$@" --user=mysql --init-file=/tmp/mysql-init.sql
   fi
-  chown -R mysql:mysql /var/lib/mysql
-
-  echo "Installing mroonga ..."
-  mysqld --user=$MYSQL_EXEC_USER &
-  sleep 5
-  mysql -u root < /usr/share/mroonga/install.sql
-  ps aux | grep "mysqld" | grep -v grep | awk '{ print $2  }' | xargs kill -KILL
-  echo "Finished installing mroonga"
-  set -- "$@" --user=$MYSQL_EXEC_USER
 fi
 
 exec "$@"
